6a1f1ffd376d4e906563b0b6c0b0e9f641c9d24b
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-04-12 23:00:10 +0200

    adds tasks 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import user from './user.json';
+import statisticalData from './statistical-data.json';
+import Profile from './components/Profile';
+import Statistics from './components/Statistics';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Profile
+      name={user.name}
+      avatar={user.avatar}
+      tag={user.tag}
+      location={user.location}
+      stats={user.stats}
+    />
+
+    <Statistics title="Upload stats" stats={statisticalData} />
+  </div>
+);
 
 export default App;
---
+++ src/components/Statistics.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Statistics = ({ title, stats }) => (
+  <section className="statistics">
+    {title && <h2 className="title">{title}</h2>}
+
+    <ul className="stat-list">
+      {stats.map(item => (
+        <li key={item.id} className="item">
+          <span className="label">{item.label}</span>
+          <span className="percentage">{item.percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
+
+export default Statistics;
