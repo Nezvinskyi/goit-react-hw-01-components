ba51fabfcb596bf9a498be422a3ee9c8dbd951d5
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-04-13 15:54:41 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import user from './user.json';
-import friends from './friends.json';
-import statisticalData from './statistical-data.json';
 import Profile from './components/Profile';
 import Statistics from './components/Statistics';
+import statisticalData from './statistical-data.json';
+import friends from './friends.json';
 import FriendList from './components/FriendList';
 import transactions from './transactions.json';
 import TransactionHistory from './components/TransactionHistory';
 
 const App = () => (
   <div>
+    <h1>HW-01-Components</h1>
+    <h2>Task 1: Profile in social network</h2>
     <Profile
       name={user.name}
       avatar={user.avatar}
@@ -18,11 +19,13 @@ const App = () => (
       location={user.location}
       stats={user.stats}
     />
-
+    <h2>Task 2: Statistics section</h2>
     <Statistics title="Upload stats" stats={statisticalData} />
 
+    <h2>Task 3: Friends list</h2>
     <FriendList friends={friends} />
 
+    <h2>Taks 4: Transaction history</h2>
     <TransactionHistory items={transactions} />
   </div>
 );
